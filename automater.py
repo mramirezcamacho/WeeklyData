992c9a68bd724f17485e96f7dc5c6705b6bcfdff
mramirezcamacho <mramirezcamacho_i@didiglobal.com>
2024-09-19 15:32:32 -0500

    IT DOWNLOAD SOMETHING!!

--- automater.py
+++ automater.py
@@ -1,5 +1,6 @@
 import datetime
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException
 from selenium.webdriver.common.action_chains import ActionChains
@@ -22,7 +23,17 @@ def read_first_two_lines(file_path):
 
 
 def start():
-    driver = webdriver.Chrome()
+    # Setup ChromeOptions to suppress logs
+    chrome_options = Options()
+    # Suppress logs (INFO, WARNING, ERROR, FATAL)
+    chrome_options.add_argument("--log-level=3")
+
+    # Additional option to suppress DevTools and SSL errors
+    chrome_options.add_argument("--disable-logging")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--ignore-certificate-errors")
+
+    driver = webdriver.Chrome(options=chrome_options)
 
     # Open a website
     driver.get("https://bigdata.intra.didiglobal.com/analysis_platform_static/board.html#/?type=reportView&reportId=80601&source=market&showMetric=false&subReportId=162327")
@@ -145,47 +156,165 @@ def changeDate(driver):
         driver, '//*[@id="Xa6NNY6v3"]/div[2]/div[2]/div/div[1]/div/div[1]/div[2]/div/div/div/div/input', 1)
     click_button(
         driver, '//*[@id="MhrHMIXeY"]/div[3]/div[2]/div/div/div[2]/div[2]/div[1]/p/strong')
-    driver.execute_script("window.scrollTo(0, 500);")
+    click_button(
+        driver, '//*[@id="Xa6NNY6v3"]/div[2]/div[2]/div/div[2]/button')
+    driver.execute_script("window.scrollTo(0, 700);")
 
 
-def hover_and_click(driver, hover_xpath, click_xpath=None, timeout=10):
+def generalClickerUsingHover(driver, hover_place_xpath: list, button_xpath: str, moreButtons: list):
+    # Wait for the hover element to be present
     try:
-        # Wait until the hover element is present
-        hover_element = WebDriverWait(driver, timeout).until(
-            EC.presence_of_element_located((By.XPATH, hover_xpath))
-        )
-
-        # Initialize ActionChains object
+        hoverElements = []
+        for x in hover_place_xpath:
+            hover_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, x))
+            )
+            hoverElements.append(hover_element)
+        # Initialize ActionChains for the driver
         actions = ActionChains(driver)
 
-        # Perform the hover action
-        actions.move_to_element(hover_element).perform()
-        print("Hovering completed.")
+        # Hover over the specified element
+        for x in hoverElements:
+            actions.move_to_element(x).perform()
 
-        # If a click_xpath is provided, wait for the click element and click after hovering
-        if click_xpath:
-            click_element = WebDriverWait(driver, timeout).until(
-                EC.element_to_be_clickable((By.XPATH, click_xpath))
-            )
-            click_element.click()
-            print("Element clicked after hovering.")
-        else:
-            print("No click element provided, only hovering.")
+        # Wait for the button to be visible after hovering
+        button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, button_xpath))
+        )
 
-    except NoSuchElementException:
-        print(f"""Element with XPath {
-              hover_xpath} not found, waiting for it to load.""")
+        # Click the button
+        button.click()
+
+        for timeS, buttons in moreButtons:
+            try:
+                time.sleep(timeS)
+                click_button(
+                    driver,  buttons)
+            except:
+                time.sleep(timeS)
+                click_button(
+                    driver,  buttons)
     except Exception as e:
-        print(f"An error occurred: {e}")
-        time.sleep(5)  # Sleep for 5 seconds and retry
+        print(f"Error: {e}")
+
+
+def setFiltersCityName(driver, hover_place_xpath: list, button_xpath):
+    # Wait for the hover element to be present
+    try:
+        hoverElements = []
+        for x in hover_place_xpath:
+            hover_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, x))
+            )
+            hoverElements.append(hover_element)
+        # Initialize ActionChains for the driver
+        actions = ActionChains(driver)
+
+        # Hover over the specified element
+        for x in hoverElements:
+            actions.move_to_element(x).perform()
+
+        # Wait for the button to be visible after hovering
+        button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, button_xpath))
+        )
+
+        # Click the button
+        button.click()
+        time.sleep(2)
+        click_button(
+            driver,  '/html/body/div[193]/div[2]/div/div/div/div[2]/div[2]/div[1]/div[3]/div[3]/label/span/input')
+        time.sleep(5)
+        click_button(
+            driver, '/html/body/div[193]/div[2]/div/div/div/div[3]/button[3]')
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def setFiltersWeek(driver, hover_place_xpath: list, button_xpath):
+    # Wait for the hover element to be present
+    try:
+        hoverElements = []
+        for x in hover_place_xpath:
+            hover_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, x))
+            )
+            hoverElements.append(hover_element)
+        # Initialize ActionChains for the driver
+        actions = ActionChains(driver)
+
+        # Hover over the specified element
+        for x in hoverElements:
+            actions.move_to_element(x).perform()
+
+        # Wait for the button to be visible after hovering
+        button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, button_xpath))
+        )
+
+        # Click the button
+        button.click()
+        time.sleep(5)
+        click_button(
+            driver, '/html/body/div[194]/ul/li[2]')
+        time.sleep(10)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def downloadData(driver, hover_place_xpath: list, button_xpath):
+    # Wait for the hover element to be present
+    try:
+        hoverElements = []
+        for x in hover_place_xpath:
+            hover_element = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, x))
+            )
+            hoverElements.append(hover_element)
+        # Initialize ActionChains for the driver
+        actions = ActionChains(driver)
+
+        # Hover over the specified element
+        for x in hoverElements:
+            actions.move_to_element(x).perform()
+
+        # Wait for the button to be visible after hovering
+        button = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, button_xpath))
+        )
+
+        # Click the button
+        button.click()
+
+        time.sleep(12)
+
+        click_button(driver, '/html/body/div[7]/div[2]/div/div/a/i')
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def downloadDailyOrders(driver):
+    generalClickerUsingHover(driver, ['//*[@id="jDYt9enkQ"]/div[3]/div[2]/div/div[1]/div[2]/div[5]/div[1]/div[4]/div[1]/canvas',],
+                             '//*[@id="jDYt9enkQ"]/div[2]/div/div[2]/div/div[1]/div/div/div/span',
+                             [(2, '/html/body/div[193]/div[2]/div/div/div/div[2]/div[2]/div[1]/div[3]/div[3]/label/span/input'), (5, '/html/body/div[193]/div[2]/div/div/div/div[3]/button[3]')])
+    time.sleep(5)
+    generalClickerUsingHover(driver, ['//*[@id="jDYt9enkQ"]/div[3]/div[2]/div/div[1]/div[2]/div[5]/div[1]/div[4]/div[1]/canvas',],
+                             '//*[@id="jDYt9enkQ"]/div[2]/div/span/div/div/a', [(5, '/html/body/div[194]/ul/li[2]')])
+
+    time.sleep(15)
+    generalClickerUsingHover(driver, ['//*[@id="jDYt9enkQ"]/div[3]/div[2]/div/div[1]/div[2]/div[5]/div[1]/div[4]/div[1]/canvas', '//*[@id="jDYt9enkQ"]/div[2]/div/div[1]/div/div'],
+                             '/html/body/div[450]/ul/li[1]', [(12, '/html/body/div[7]/div[2]/div/div/a/i')])
 
 
 def body(driver):
     enterDiDiDashboard(driver)
     changeDate(driver)
     time.sleep(5)
-    hover_and_click(driver, '//*[@id="slickgrid_5349491"]/span',
-                    '//*[@id="jDYt9enkQ"]/div[2]/div/div[2]/div/div[1]/div/div/div')
+    downloadDailyOrders(driver)
+    time.sleep(100)
 
 
 if __name__ == "__main__":
